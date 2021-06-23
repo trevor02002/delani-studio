5f0bdbab0ee995900c6a4b7c823854dcbb5a75fc
trevor02002 <85225623+trevor02002@users.noreply.github.com>
2021-06-23 03:49:18 -0400

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,36 @@
+$(document).click(function () {
+    $("#img1").click(function () {
+        $("#pe1").toggle();
+        $("#img1").toggle();
+    })
+    $("#pe1").click(function () {
+        $("#pe1").toggle();
+        $("#img1").toggle();
+    })
+    $("#img2").click(function () {
+        $("#pe2").toggle();
+        $("#img2").toggle();
+    })
+    $("#pe2").click(function () {
+        $("#pe2").toggle();
+        $("#img2").toggle();
+    })
+    $("#img3").click(function () {
+        $("#pe3").toggle();
+        $("#img3").toggle();
+    })
+    $("#pe3").click(function () {
+        $("#pe3").toggle();
+        $("#img3").toggle();
+    })
+})
+// (document).click(function () {
+//     $("#img2").click(function () {
+//         $("#pe2").toggle();
+//         $("#img").toggle();
+//     })
+//     $("#pe1").click(function () {
+//         $("#pe2").toggle();
+//         $("#img2").toggle();
+//     })
+// })
